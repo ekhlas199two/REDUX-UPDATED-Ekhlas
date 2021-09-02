60cfb4bdf0468e8e2b74a4c97243c0915aec2b5f
ekhlas199two <68354941+ekhlas199two@users.noreply.github.com>
2021-09-02 15:28:41 +0100

    first commit

---
+++ src/REDUX/actions/todoactions.js
@@ -0,0 +1,20 @@
+import {ADDTASK,DELETETASK,EDITTASK,TASKDONE} from '../types/todotypes'
+
+export const addtask = (Text) => {
+    return { type: ADDTASK, payload: Text };
+  };
+
+  export const taskdone=(id)=>{
+    return { type: TASKDONE, payload: id };
+  };
+
+// export const edittask=(id)=>{
+//     return {type: EDITTASK, payload:id}
+// };
+
+export const edittask = (id,payload) => {
+  return { type: EDITTASK,id, payload };
+};
+export const deletetask =(payload)=>{
+    return {type:DELETETASK, payload}
+}
